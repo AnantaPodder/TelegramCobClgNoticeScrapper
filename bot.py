9b07b72f782ba195f718fe1c23a136635a356982
Ananta Podder <ananta8967940712@gmail.com>
2022-02-25 05:48:00 +0530

    edited more config asd

--- bot.py
+++ bot.py
@@ -5,6 +5,19 @@ import time
 import requests
 from config import telegram_bot_api
 
+### for heroku ###
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(
+    executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options
+)
+
+#### for heroku ###
 driver = webdriver.Chrome("./chromedriver")
 
 driver.implicitly_wait(5)
