33503c014715d0470c34cbda512e996c663857da
Ananta Podder <ananta8967940712@gmail.com>
2022-02-25 05:39:21 +0530

    edited config files

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-telegram_bot_api = 
-host = 
-user = 
-password = 
-database = 
+import os
+
+telegram_bot_api = os.environ.get("telegram_bot_api")
+host = os.environ.get("host")
+user = os.environ.get("user")
+password = os.environ.get("password")
+database = os.environ.get("database")
